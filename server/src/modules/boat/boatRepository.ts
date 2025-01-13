4a471448de4a39184df9caa49cd43d8ef737d818
CAMILLEG <cgyrd@outlook.fr>
2025-01-13 13:23:19 +0100

    oups j'avais d'inserer mcd

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -25,7 +25,7 @@ class BoatRepository {
     const { id, coord_x, coord_y } = boatToUpdate;
     const [result] = await databaseClient.query<Result>(
       "UPDATE boat SET coord_x = ?, coord_y = ? WHERE id = ?",
-      [coord_x, coord_y, id]
+      [coord_x, coord_y, id],
     );
     return result.affectedRows;
   }
--- server/src/router.ts
+++ server/src/router.ts
@@ -11,7 +11,7 @@ router.get("/api/tiles", tileActions.browse);
 
 import boatActions from "./modules/boat/boatActions";
 router.get("/api/boats", boatActions.browse);
-router.put ("/api/boats/:id" , boatActions.edit)
+router.put("/api/boats/:id", boatActions.edit);
 
 import gameActions from "./modules/game/gameActions";
 
