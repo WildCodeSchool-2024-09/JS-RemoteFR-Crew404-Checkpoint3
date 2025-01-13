1813402be85fb52c92194abda289c930fecc865b
Charles Catto <charlescatto@outlook.fr>
2025-01-13 11:59:41 +0100

    Fixing the remaining issues of step4.

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,3 +1,44 @@
+// import type { RequestHandler } from "express";
+// import tileRepository from "./tileRepository";
+
+// const browse: RequestHandler = async (req, res, next) => {
+//   try {
+//     const tiles = await tileRepository.readAll();
+//     res.json(tiles);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// const validate: RequestHandler = async (req, res, next) => {
+//   const { coord_x, coord_y } = req.body;
+//   //   // your code here
+//   //   res.status(501).json({ message: "Validation nope.avi" });
+//   // };
+
+//   if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+//     res.status(422).json({ message: "Invalid coordinates" });
+//     return;
+//   }
+//   try {
+//     const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+//     if (!tile) {
+//       res.status(422).json({ message: "Tile not found" });
+//       return;
+//     }
+
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// export default {
+//   browse,
+//   validate,
+// };
+
 import type { RequestHandler } from "express";
 import tileRepository from "./tileRepository";
 
@@ -10,21 +51,19 @@ const browse: RequestHandler = async (req, res, next) => {
   }
 };
 
-const validate: RequestHandler = async (req, res, next) => {
+const validate: RequestHandler = async (req, res, next): Promise<void> => {
   const { coord_x, coord_y } = req.body;
-  //   // your code here
-  //   res.status(501).json({ message: "Validation nope.avi" });
-  // };
 
   if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
-    res.status(422).json({ message: "Invalid coordinates" });
+    res.sendStatus(422);
     return;
   }
+
   try {
     const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
 
     if (!tile) {
-      res.status(422).json({ message: "Tile not found" });
+      res.sendStatus(422);
       return;
     }
 
