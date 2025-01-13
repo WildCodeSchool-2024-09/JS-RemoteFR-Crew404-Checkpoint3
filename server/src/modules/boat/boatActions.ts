bfa8c95a6673e694085faa008e733599e4c5d158
CAMILLEG <cgyrd@outlook.fr>
2025-01-13 13:14:17 +0100

    check 3- envie de crever

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,16 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boat = req.body;
+    const id = req.params.id;
+
+    await boatRepository.update(boat);
+
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
