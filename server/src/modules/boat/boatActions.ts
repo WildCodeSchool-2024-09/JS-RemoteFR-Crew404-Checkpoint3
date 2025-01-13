8896b527a8dc0d1c9e434dfae83a77ebce897deb
Manon Moutier <manon.moutier@hotmail.fr>
2025-01-13 09:55:44 +0100

    step 3 done

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -17,6 +17,22 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const edit: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    // Extract the ID from the request parameters
+    const id = req.params.id;
+
+    // Extract the boat data from the request body
+    const boat = req.body;
+
+    // Update the boat in the database
+    await boatRepository.update(boat);
+
+    // Respond with a 204 status code
+    res.sendStatus(204);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 export default {
--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -22,7 +22,12 @@ class BoatRepository {
 
   async update(boatToUpdate: Partial<Boat>) {
     // your code here
-    return 0;
+    const { id, coord_x, coord_y } = boatToUpdate;
+    const [result] = await databaseClient.query<Result>(
+      "update boat set coord_x = ?, coord_y = ? where id = ?",
+      [coord_x, coord_y, id],
+    );
+    return result.affectedRows;
   }
 }
 
