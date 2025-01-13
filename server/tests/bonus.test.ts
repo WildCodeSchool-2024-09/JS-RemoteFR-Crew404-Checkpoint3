b1d1156f2a8f755bbaff8e18bffade2a70a77eec
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-01-13 16:12:28 +0100

    step bonus corrected done

--- server/tests/bonus.test.ts
+++ server/tests/bonus.test.ts
@@ -3,14 +3,16 @@ import "dotenv/config";
 
 import supertest from "supertest";
 
+import databaseClient from "../database/client";
+
 import app from "../src/app";
 import boatRepository from "../src/modules/boat/boatRepository";
 
 describe("GET /api/boats?name=Black Pearl", () => {
-  // test("you added a 'where' parameter to method readAll() in BoatRepository", async () => {
-  // 	expect(boatRepository.readAll()).toHaveLength(1);
-  // });
-  test("your method readAll() returns all boats if where == null", async () => {
+  test("you added a 'where' parameter to method readAll() in BoatRepository, which is null by default", async () => {
+    expect(boatRepository.readAll).toHaveLength(0);
+  });
+  test("your method readAll() returns all boats if where is null", async () => {
     const rows = await boatRepository.readAll();
 
     expect(rows).toHaveLength(4);
@@ -20,11 +22,15 @@ describe("GET /api/boats?name=Black Pearl", () => {
 
     expect(rows).toHaveLength(1);
   });
-  test("you passed 'req.query' as argument to boatRepository.readAll() in boatActions.js", async () => {
+  test("you used 'req.query' to build a where object passed as argument to boatRepository.readAll() in boatActions.js", async () => {
     const response = await supertest(app).get("/api/boats?name=Black%20Pearl");
 
     expect(response.status).toBe(200);
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.body).toHaveLength(1);
   });
+});
+
+afterAll((done) => {
+  databaseClient.end().then(done);
 });
