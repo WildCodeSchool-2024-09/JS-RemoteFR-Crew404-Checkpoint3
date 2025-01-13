525405778dc7af642f31e65b94e8c45f449a20d5
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-01-13 14:49:07 +0100

    step3 done 🤟

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -17,6 +17,30 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const edit: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    // Extract the ID from the request parameters
+    const id = Number.parseInt(req.params.id, 10);
+
+    // Extract the boat data from the request body
+    const boat = {
+      id,
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    // Update the boat in the database
+    const result = await boatRepository.update(boat);
+
+    if (result === 0) {
+      // Respond with a 204 status code
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 export default {
