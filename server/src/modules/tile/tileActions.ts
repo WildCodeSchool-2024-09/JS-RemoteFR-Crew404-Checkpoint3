a2a6d5b9057fab94341affcdf327f1aba2e1d309
Anthony Gorski <anthony.developpeurweb@gmail.com>
2025-01-13 15:03:38 +0100

    step4 done 🚀

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -12,7 +12,25 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const { coord_x, coord_y } = req.body;
+
+    // Si galère avec le repository, astuce de derriere les fagots
+    // if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+    // 	res.sendStatus(422);
+    // 	return;
+    // }
+
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (tile.length !== 0) {
+      next();
+    } else {
+      res.sendStatus(422);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
--- server/src/modules/tile/tileRepository.ts
+++ server/src/modules/tile/tileRepository.ts
@@ -22,7 +22,13 @@ class TileRepository {
   }
 
   async readByCoordinates(coordX: number, coordY: number) {
-    // your code here
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from tile where coord_x = ? and coord_y = ?",
+      [coordX, coordY],
+    );
+
+    // Return the array of tiles
+    return rows as Tile[];
   }
 
   async getRandomIsland() {
