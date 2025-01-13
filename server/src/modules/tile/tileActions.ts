81be9e1a034efa55497b8e04eb0e6569c6f8d0ee
Charles Catto <charlescatto@outlook.fr>
2025-01-13 11:31:50 +0100

    Attempting step4.

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -11,8 +11,27 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
-  res.status(501).json({ message: "Validation nope.avi" });
+  const { coord_x, coord_y } = req.body;
+  //   // your code here
+  //   res.status(501).json({ message: "Validation nope.avi" });
+  // };
+
+  if (coord_x < 0 || coord_x > 11 || coord_y < 0 || coord_y > 5) {
+    res.status(422).json({ message: "Invalid coordinates" });
+    return;
+  }
+  try {
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+    if (!tile) {
+      res.status(422).json({ message: "Tile not found" });
+      return;
+    }
+
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
